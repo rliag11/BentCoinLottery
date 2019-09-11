b905ebfa521734417a14ced5fc0427b0945aba5d
Armun <liaghat.armun@gmail.com>
2019-09-11 14:35:50 -0400

    Commented and changed variable names
    Explicitly indicated blocks of code, more indicative var names, commented explicitly on what code is doing

--- lottery.R
+++ lottery.R
@@ -9,17 +9,20 @@
 
 library(stats)
 
-# Small edit! from class 1!
-
-arr <- array(0,10)
+#Example of one lottery ticket
+#the following block of code iterates the 10 numbers of a lottery ticket
+ticket <- array(0,10) #initialize array of ticket's 10 numbers
 for (i in 1:10){
-  arr[i] <- if (runif(1) < 0.1) 1 else 0
+  ticket[i] <- if (runif(1) < 0.5) 1 else 0 #equivalent to coin flip. 
+  #distribution giving less than 1/2 is eequivalent to give one side of coin. 
+  #we define 1 as heads
 }
 
+#Following block iterates 100 lotter tickets: each with an array of 10 numbers.
 for (j in 1:100){
-  arr <- array(0,10)
+  ticket <- array(0,10)
   for (i in 1:10){
-    arr[i] <- if (runif(1) < 0.1) 1 else 0
+    ticket[i] <- if (runif(1) < 0.5) 1 else 0 #See former block of example code
   }
-  print(arr)
+  print(ticket)
 }
